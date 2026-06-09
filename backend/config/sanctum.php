ba4ed6d91d1eaaf9251be1e2a88debff5598faf9
bayzal <saliouniass191817@gmail.com>
2026-06-09 16:35:55 +0000

    tout est bon

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -6,21 +6,22 @@ $frontendDomain = $frontendHost ? $frontendHost.($frontendPort ? ':'.$frontendPo
 
 return [
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', implode(',', array_filter([
-        'localhost',
-        'localhost:5173',
-        '127.0.0.1',
-        '127.0.0.1:5173',
-        $frontendHost,
-        $frontendDomain,
-    ])))),
+            'localhost',
+                    'localhost:5173',
+                            '127.0.0.1',
+                                'app-web-yoonema.vercel.app',
+                                    '127.0.0.1:5173',
+                                            $frontendHost,
+                                                    $frontendDomain,
+                                                        ])))),
 
-    'guard' => ['web'],
-    'expiration' => null,
-    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
+                                                            'guard' => ['web'],
+                                                                'expiration' => null,
+                                                                    'token_prefix' => env('SANCTUM_TOKEN_PREFIX', ''),
 
-    'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
-    ],
-];
+                                                                        'middleware' => [
+                                                                                'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+                                                                                        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
+                                                                                                'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+                                                                                                    ],
+                                                                                                    ];
